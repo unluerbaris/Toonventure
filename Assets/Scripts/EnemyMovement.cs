ff41fcd1c69e7e827373d942f8a1019016ab0fb8
unluerbaris <unluerbaris@gmail.com>
2019-07-12 00:06:03 +0300

    Changes on Player movements and a new enemy with ground detection

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Transform groundDetection;
+    [SerializeField] float groundDetectionRayDistance = 2f;
+    private bool movingLeft = true;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundDetectionRayDistance);
+        if (groundInfo.collider == false)
+        {
+            if (movingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,36 +9,29 @@ public class Player : MonoBehaviour
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpForce = 20f;
     [SerializeField] float climbSpeed = 5f;
-    [SerializeField] float dashSpeed = 9f;
 
     bool isAlive = true;
 
     Rigidbody2D myRigidbody;
     Animator myAnimator;
-    Collider2D myCollider2d;
+    CapsuleCollider2D myBodyCollider2d;
+    BoxCollider2D myFeetCollider;
     float gravityScaleAtStart;
-    float startValueOfRunSpeed;
-    float onTheAirTime;
-    float glideOnTheAirTimeLimit = 0.5f; // Between 0-0.5secs while on the air
-                                        // dashSpeed on X axis can effect while Jumping or Gliding 
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2d = GetComponent<Collider2D>();
+        myBodyCollider2d = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidbody.gravityScale;
-        startValueOfRunSpeed = runSpeed;
     }
 
     void Update()
     {
         Run();
-        Sprint();
         Jump();
-        OnTheAirTime();
         Climb();
-        Debug.Log(onTheAirTime);
     }
 
     private void Run()
@@ -46,76 +39,47 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); //-1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
-        RunningAnimationAndFlipSprite();
-    }
-
-    private void Sprint()
-    {
-        if (Input.GetButton("Dash") && onTheAirTime <= glideOnTheAirTimeLimit)
-        {
-            runSpeed = dashSpeed; 
-        }
-        else
-        {
-            runSpeed = startValueOfRunSpeed;
-        }
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("running", playerHasHorizontalSpeed);
+        FlipSprite();
     }
 
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && myCollider2d.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if (Input.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocity = new Vector2(0f, jumpForce);
             myRigidbody.velocity += jumpVelocity;
         }
     }
 
-    private void OnTheAirTime()
+    private void Climb()//TODO check jump on the ladder action again
     {
-        if (myCollider2d.IsTouchingLayers(LayerMask.GetMask("Ground")) || 
-            myCollider2d.IsTouchingLayers(LayerMask.GetMask("Climbing")))
-        {
-            onTheAirTime = 0f;
-            return;
-        }
-        onTheAirTime += Time.deltaTime;
-    }
-
-    private void Climb()
-    {
-        if (!myCollider2d.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")) || Input.GetButtonDown("Jump"))
         {
             myAnimator.SetBool("climbing", false);
             myRigidbody.gravityScale = gravityScaleAtStart;
             return;
         }
-        myRigidbody.gravityScale = 0;
         float climbThrow = Input.GetAxis("Vertical"); //-1 to +1
         Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, climbThrow * climbSpeed);
         myRigidbody.velocity = climbVelocity;
-        ClimbingAnimation();
-    }
+        myRigidbody.gravityScale = 0f;
 
-    private void RunningAnimationAndFlipSprite()
-    {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed && !myCollider2d.IsTouchingLayers(LayerMask.GetMask("Climbing")))
-        {
-            myAnimator.SetBool("running", true); //Running animation is active
-            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
-        }
-        else
-        {
-            myAnimator.SetBool("running", false); // Stops to play running animation
-        }
-    }
-
-    private void ClimbingAnimation()
-    {
         bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
         if (playerHasVerticalSpeed)
         {
-            myAnimator.SetBool("climbing", true);
+            myAnimator.SetBool("climbing", playerHasVerticalSpeed);
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
     }
 }
