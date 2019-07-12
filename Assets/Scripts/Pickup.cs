35b21ff122e7109f97d8cc3899efb6924788565f
unluerbaris <unluerbaris@gmail.com>
2019-07-12 15:25:03 +0300

    Picups and SFX

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] AudioClip carrotPickUpSFX;
+    [Range(0,1)][SerializeField] float soundVol = 0.25f;
+    GameObject audioListener;
+
+    private void Start()
+    {
+        audioListener = GameObject.FindWithTag("AudioListener");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(carrotPickUpSFX, audioListener.transform.position, soundVol);
+        Destroy(gameObject);
+    }
+}
