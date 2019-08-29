b497ab983134868b366a5d21f9aaeaf4ae7a8a50
unluerbaris <unluerbaris@gmail.com>
2019-08-29 23:10:05 +0900

    Bomber bee

---
+++ Assets/Scripts/Enemy/Bomber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomber : MonoBehaviour
+{
+    [SerializeField] GameObject bomb;
+
+    void Start()
+    {
+        InvokeRepeating("ThrowBomb", 1, 1);
+    }
+
+    private void ThrowBomb()
+    {
+        GameObject bombInstance = Instantiate(bomb,
+                                              transform.position,
+                                              Quaternion.identity) as GameObject;
+    }
+}
---
+++ Assets/Scripts/Projectiles/Explode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    Animator animator;
+    GameObject audioListener;
+    [SerializeField] AudioClip explosionSFX;
+    [Range(0, 1)] [SerializeField] float explosionVolume = 0.25f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioListener = GameObject.FindWithTag("AudioListener");
+    }
+
+    void Update()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(explosionSFX, audioListener.transform.position, explosionVolume);
+            animator.SetTrigger("explode");
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
