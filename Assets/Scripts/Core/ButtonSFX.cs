d17d41e68eedd4fe7e697eb4a953a727223aa308
unluerbaris <unluerbaris@gmail.com>
2019-08-31 16:49:18 +0900

    Button SFX

---
+++ Assets/Scripts/Core/ButtonSFX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFX : MonoBehaviour
+{
+    [SerializeField] AudioClip hoverSFX;
+    [Range(0, 1)] [SerializeField] float hoverSFXVolume = 1f;
+    [SerializeField] AudioClip clickSFX;
+    [Range(0, 1)] [SerializeField] float clickSFXVolume = 1f;
+
+    GameObject audioListener;
+
+    private void Start()
+    {
+        audioListener = GameObject.FindWithTag("AudioListener");
+    }
+
+    public void PlayHoverSFX()
+    {
+        if (hoverSFX != null)
+        {
+            AudioSource.PlayClipAtPoint(hoverSFX, audioListener.transform.position, hoverSFXVolume);
+        }
+    }
+
+    public void PlayClickSFX()
+    {
+        if (clickSFX != null)
+        {
+            AudioSource.PlayClipAtPoint(clickSFX, audioListener.transform.position, clickSFXVolume);
+        }
+    }
+}
