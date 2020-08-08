aebf611d491ac53daac01821771c9b47d9ff4c93
Baris Unluer <unluerbaris@gmail.com>
2020-08-08 14:50:38 +0900

    game level UI

---
+++ Assets/Scripts/UI/ButtonSFX.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Toon.Core;
+
+namespace Toon.UI
+{
+    public class ButtonSFX : MonoBehaviour
+    {
+        AudioManager audioManager;
+
+        private void Start()
+        {
+            audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+        }
+
+        public void PlayHoverSFX()
+        {
+            audioManager.PlaySound("hover");
+        }
+
+        public void PlayClickSFX()
+        {
+            audioManager.PlaySound("click");
+        }
+    }
+}
